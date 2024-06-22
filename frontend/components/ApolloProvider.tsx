a25dae819c9b1774de5cb2dedf6acd45b6828fb1
yoritin <47884591+yoritin@users.noreply.github.com>
2024-06-22 17:40:34 +0900

    fix layout

---
+++ frontend/components/ApolloProvider.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ApolloProvider as Provider } from '@apollo/client'
+import client from '../lib/apolloClient'
+
+export default function ApolloProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <Provider client={client}>{children}</Provider>
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,34 @@
+'use client'
 import styles from './page.module.css'
+import { gql, useQuery } from '@apollo/client'
+
+const PLANTS = gql`
+  query {
+    plants {
+      name
+      description
+    }
+  }
+`
+
+const Plants = () => {
+  const { loading, error, data } = useQuery(PLANTS)
+  if (loading) return <>loading</>
+  if (error) return <>error</>
+  return data.plants.map((plant: any) => {
+    return (
+      <div key={plant.name}>
+        <p>{plant.name}</p>
+      </div>
+    )
+  })
+}
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>GraphQL</p>
-      </div>
+      <h1>GraphQL</h1>
+      <Plants />
     </main>
   )
 }
