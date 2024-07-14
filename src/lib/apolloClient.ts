b08edf8115708322ed82981eb52cb1bec6448876
yoritin <47884591+yoritin@users.noreply.github.com>
2024-07-14 18:55:43 +0900

    add env (#3)

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri:
+    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
 })
 
